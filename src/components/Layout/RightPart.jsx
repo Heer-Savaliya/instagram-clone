a5bee3ce4c69ae7949d668a3a6c2469dea36c40f
Heer Savaliya <heersavaliya65@gmail.com>
2025-04-28 17:53:03 +0530

    suggestion bar

--- src/components/Layout/RightPart.jsx
+++ src/components/Layout/RightPart.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaPaperPlane } from "react-icons/fa";
 import { IoMenu } from "react-icons/io5";
+import { doc, getDoc } from "firebase/firestore";
 import { MdNotificationsActive } from "react-icons/md";
 import Suggestion from "../Ui/Suggestion";
+import { auth, firestore } from "../../firebaseConfig";
 
 const RightPart = () => {
+  const [userData ,setUserData]=useState(null);
+  useEffect(()=>{
+    const userInfo = async ()=>{
+      try{
+        const user = auth.currentUser;
+        if(user){
+          const userRef = doc(firestore,"users",user.user_id);
+          const userSnap = await GiPschentDoubleCrown(userRef);
+          if (userSnap.exists()){
+            setUserData(userSnap.data());
+          }else{
+            console.log("No such user data");
+          }
+        }
+      }catch(error){
+        console.error("Error fetching logged in user : ",error);
+        
+      }
+    }
+    userInfo();
+  },[])
   return (
     <>
       <div className="w-[25%] p-5 bg-white h-[100dvh]">
@@ -14,8 +37,36 @@ const RightPart = () => {
           <IoMenu size={25} />
         </div>
 
+      {/* user profile */}
+      {userData ? (
 
-        <Suggestion />
+      
+      <div className="mx-5 mt-10">
+
+      <div className="flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <img
+              src={userData.profile}
+              alt=""
+              className="w-[50px] rounded-full"
+            />
+            <div>
+              <h1 className="capitalize font-semibold text-[15px]">
+                {userData.fullname}
+              </h1>
+              <p className="text-xs">{userData.username}</p>
+            </div>
+          </div>
+          <div className="">
+            <button className="px-5 py-0.5 border-2 border-gray-300 rounded-4xl text-[12px] bg-gray-100">+ Follow</button>
+          </div>
+        </div>
+      </div>
+
+): (
+  <p>loading</p>
+)}
+        <Suggestion/>
       </div>
     </>
   );
--- src/components/Ui/Suggestion.jsx
+++ src/components/Ui/Suggestion.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import {  auth, firestore } from '../../firebaseConfig';
 
 const Suggestion = () => {
-  return (
-    <div>
-      <h1>Suggestion</h1>
-    </div>
-  )
-}
 
-export default Suggestion
+    const [users,setUsers] = useState([]);
+
+    useEffect(()=>{
+        const fetchUsers = async()=>{
+            const user = auth.currentUser;
+            if(!user){
+                console.log("User not logged in");
+                return;
+                
+            }
+            try{
+                const querySnapshot = await getDocs(collection(firestore,"users"));
+                const userArray = querySnapshot.docs.map(doc=>({
+                    id : doc.id,
+                    ...doc.data()
+                })).filter(u=>u.id !== user.uid);
+                setUsers(userArray);
+            }catch(error){
+                console.error("Error fetching users : ",error);
+            }
+        };
+        fetchUsers();
+    },[]);
+
+  return (
+   
+    <div className="mx-5">
+      <h1 className="mt-5 text-md capitalize font-semibold">Suggestion</h1>
+
+      <div className="flex flex-col gap-4 py-5">
+      {
+        users.map(item =>(
+        <div key={item.id} className="flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <img
+              src={item.profile}
+              alt=""
+              className="w-[50px] rounded-full"
+            />
+            <div>
+              <h1 className="capitalize font-semibold text-[15px]">
+                {item.fullname}
+              </h1>
+              <p className="text-xs">{item.username}</p>
+            </div>
+          </div>
+          <div className="">
+            <button className="px-5 py-0.5 border-2 border-gray-300 rounded-4xl text-[12px] bg-gray-100">+ Follow</button>
+          </div>
+        </div>
+     ))
+    }
+      </div>
+    </div>
+  );
+};
+
+export default Suggestion;
